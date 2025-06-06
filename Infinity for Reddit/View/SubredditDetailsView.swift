244292e4bd729f084a5b37ac49ac3b9b06c32374
joeylr2042 <55361670+joeylr2042@users.noreply.github.com>
2025-06-06 16:57:31 -0400

    Continue to implement the stretchy header and the subreddit name showing up at the top when scrolling down in SubredditDetailsView.

--- Infinity for Reddit/View/SubredditDetailsView.swift
+++ Infinity for Reddit/View/SubredditDetailsView.swift
@@ -24,6 +24,7 @@ struct SubredditDetailsView: View {
     @State private var isToolbarBackgroundVisible = true
     @State private var subscribeTask: Task<Void, Never>?
     @State private var selectedSection: ProfileSection = .posts
+    @State private var headerMinY: CGFloat = 0
     
     @StateObject var subredditDetailsViewModel : SubredditDetailsViewModel
     
@@ -37,127 +38,159 @@ struct SubredditDetailsView: View {
     }
     
     var body: some View {
-        List {
-            if let subredditData = subredditDetailsViewModel.subredditData {
-                CustomWebImage(
-                    subredditData.bannerUrl,
-                    width: UIScreen.main.bounds.width,
-                    height: 150,
-                    centerCrop: true,
-                    fallbackView: {
-                        Color(hex: themeViewModel.currentCustomTheme.colorPrimary)
-                            .frame(height: 150)
-                    }
-                )
-                .listPlainItemNoInsets()
-                
-                VStack(spacing: 0) {
-                    HStack(spacing: 0) {
-                        CustomWebImage(
-                            subredditData.iconUrl,
-                            width: 80,
-                            height: 80,
-                            circleClipped: true
-                        )
+        GeometryReader { proxy in
+            
+            List {
+                if let subredditData = subredditDetailsViewModel.subredditData {
+                    let height = 150.0
+                    GeometryReader { headerProxy in
+                        let headerMinY = headerProxy.frame(in: .named("SCROLL")).minY
+                        let dynamicHeight = max(0, 150 + headerMinY)
                         
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text("r/\(subredditData.name)")
-                                .subreddit()
+                        CustomWebImage(
+                            subredditData.bannerUrl,
+                            width: UIScreen.main.bounds.width,
+                            height: dynamicHeight,
+                            centerCrop: true,
+                            fallbackView: {
+                                Color(hex: themeViewModel.currentCustomTheme.colorPrimary)
+                                    .frame(height: dynamicHeight)
+                            }
+                        )
+                        .ignoresSafeArea(.container, edges: .top)
+                        .offset(y: -headerMinY)
+                        .onChange(of: headerMinY) {
+                            self.headerMinY = headerMinY
+                        }
+                    }
+                    .listPlainItemNoInsets()
+                    .frame(height: height)
+                    
+                    VStack(spacing: 0) {
+                        HStack(spacing: 0) {
+                            CustomWebImage(
+                                subredditData.iconUrl,
+                                width: 80,
+                                height: 80,
+                                circleClipped: true
+                            )
                             
-                            Button(action: {
-                                subscribeTask?.cancel()
-                                subscribeTask = Task {
-                                    await subredditDetailsViewModel.toggleSubscribeSubreddit()
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("r/\(subredditData.name)")
+                                    .subreddit()
+                                
+                                Button(action: {
+                                    subscribeTask?.cancel()
+                                    subscribeTask = Task {
+                                        await subredditDetailsViewModel.toggleSubscribeSubreddit()
+                                    }
+                                }) {
+                                    Text(subredditDetailsViewModel.isSubscribed ? "Subscribed" : "Subscribe")
+                                        .padding(.horizontal, 15)
+                                        .padding(.vertical, 8)
+                                        .background(Color.blue)
+                                        .foregroundColor(.white)
+                                        .cornerRadius(20)
                                 }
-                            }) {
-                                Text(subredditDetailsViewModel.isSubscribed ? "Subscribed" : "Subscribe")
-                                    .padding(.horizontal, 15)
-                                    .padding(.vertical, 8)
-                                    .background(Color.blue)
-                                    .foregroundColor(.white)
-                                    .cornerRadius(20)
+                            }
+                            .padding(.leading, 16)
+                            
+                            Spacer()
+                        }
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 16)
+                        
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text("Subscribers: \(subredditData.nSubscribers ?? 0)")
+                                    .primaryText()
+                                
+                                Spacer()
+                                
+                                Text("Since:")
+                                    .primaryText()
+                            }
+                            
+                            HStack {
+                                Text("Online: \(subredditData.activeUsers ?? 0)")
+                                    .primaryText()
+                                
+                                Spacer()
+                                
+                                Text("\(subredditDetailsViewModel.formattedCakeDay(TimeInterval(subredditData.createdUTC ?? 0)))")
+                                    .primaryText()
                             }
                         }
-                        .padding(.leading, 16)
+                        //                        .padding(.horizontal, 16)
+                        .padding(.bottom, 16)
                         
-                        Spacer()
-                    }
-                    .padding(.horizontal, 16)
-                    .padding(.vertical, 16)
-                    
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text("Subscribers: \(subredditData.nSubscribers ?? 0)")
-                                .primaryText()
-                            
-                            Spacer()
-                            
-                            Text("Since:")
-                                .primaryText()
-                        }
-                        
-                        HStack {
-                            Text("Online: \(subredditData.activeUsers ?? 0)")
-                                .primaryText()
-                            
-                            Spacer()
-                            
-                            Text("\(subredditDetailsViewModel.formattedCakeDay(TimeInterval(subredditData.createdUTC ?? 0)))")
-                                .primaryText()
+                        if subredditData.sidebarDescription?.isEmpty ?? true == false {
+                            subredditData.sidebarDescription.map {
+                                Markdown($0)
+                                    .themedMarkdown()
+                                    .padding(.leading, 4)
+                                    .padding(.bottom, 16)
+                            }
                         }
                     }
                     .padding(.horizontal, 16)
-                    .padding(.bottom, 16)
+                    .listPlainItemNoInsets()
                     
-                    if subredditData.sidebarDescription?.isEmpty ?? true == false {
-                        subredditData.sidebarDescription.map {
-                            Markdown($0)
-                                .themedMarkdown()
-                                .padding(.leading, 4)
-                                .padding(.bottom, 16)
+                    Picker("Select Section", selection: $selectedSection) {
+                        ForEach(ProfileSection.allCases) { section in
+                            Text(section.rawValue).tag(section)
                         }
                     }
-                }
-                .listPlainItemNoInsets()
-                
-                Picker("Select Section", selection: $selectedSection) {
-                    ForEach(ProfileSection.allCases) { section in
-                        Text(section.rawValue).tag(section)
+                    .pickerStyle(.segmented)
+                    .listRowInsets(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
+                    
+                    switch selectedSection {
+                    case .posts:
+                        PostListingView(
+                            account: accountViewModel.account,
+                            postListingMetadata:PostListingMetadata(
+                                postListingType:.subreddit,
+                                pathComponents: ["sortType": "hot", "subreddit": "\(subredditData.name)"],
+                                headers: APIUtils.getOAuthHeader(accessToken: accountViewModel.account.accessToken ?? ""),
+                                queries: nil,
+                                params: nil
+                            ),
+                            isRootView: false
+                        )
+                        .id(accountViewModel.account.username)
+                    case .about:
+                        SubredditAboutView(description: subredditData.description)
                     }
                 }
-                .pickerStyle(.segmented)
-                .listRowInsets(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
-                
-                switch selectedSection {
-                case .posts:
-                    PostListingView(
-                        account: accountViewModel.account,
-                        postListingMetadata:PostListingMetadata(
-                            postListingType:.subreddit,
-                            pathComponents: ["sortType": "hot", "subreddit": "\(subredditData.name)"],
-                            headers: APIUtils.getOAuthHeader(accessToken: accountViewModel.account.accessToken ?? ""),
-                            queries: nil,
-                            params: nil
-                        ),
-                        isRootView: false
-                    )
-                    .id(accountViewModel.account.username)
-                case .about:
-                    SubredditAboutView(description: subredditData.description)
+            }
+            .coordinateSpace(name: "SCROLL")
+            .edgesIgnoringSafeArea(.top)
+            .themedList()
+            .themedNavigationBar()
+            .task {
+                if subredditDetailsViewModel.subredditData == nil {
+                    await subredditDetailsViewModel.fetchSubredditDetails()
                 }
             }
-        }
-        .edgesIgnoringSafeArea(.top)
-        .themedList()
-        .themedNavigationBar()
-        .task {
-            if subredditDetailsViewModel.subredditData == nil {
-                await subredditDetailsViewModel.fetchSubredditDetails()
+            .toolbarBackground(.hidden, for: .navigationBar)
+            .toolbar {
+                NavigationBarMenu()
             }
-        }
-        .toolbarBackground(.hidden, for: .navigationBar)
-        .toolbar {
-            NavigationBarMenu()
+            .overlay(alignment: .top) {
+                let scrollThreshold: CGFloat = 100.0
+                let opacity = min(1, max(0, (-headerMinY / scrollThreshold)))
+                
+                ZStack {
+                    Color(hex: themeViewModel.currentCustomTheme.colorPrimary)
+                    
+                    Text("r/\(subredditDetailsViewModel.subredditData?.name ?? "")")
+                        .navigationBarPrimaryText()
+                        .padding(.top, proxy.safeAreaInsets.top)
+                }
+                .frame(height: (proxy.safeAreaInsets.top) + 30)
+                .opacity(opacity)
+                .ignoresSafeArea()
+            }
         }
     }
 }
